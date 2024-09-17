3d8e4bc959cf04ec01f1f72e3bbb23efc4c426f4
Allan Sanyanga <87567534+allansanyaz@users.noreply.github.com>
2024-09-17 22:09:10 +0200

    babel fix for jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,11 +16,7 @@ test('Checking to see if the page is loading', () => {
 test('Fetches the robofriends data from the server', async() => {
     renderWithProviders(<App />)
     // perform a request to the server using the fetch api and jest
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data = await response.json()
-        await expect(screen.getByText(`${data[0].name}`)).toBeInTheDocument();
-    } catch (error) {
-        expect(error).toBeNull();
-    }
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json()
+    await expect(screen.getByText(`${data[0].name}`)).toBeInTheDocument();
 });
