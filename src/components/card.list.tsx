f4f52be3f8601f88ea3537b16ba7cbe0c73a8760
Allan Sanyanga <87567534+allansanyaz@users.noreply.github.com>
2024-09-17 21:03:50 +0200

    tired of these tests

--- src/components/card.list.tsx
+++ src/components/card.list.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./card";
+import Card from "./Card";
 import { IRobotState } from "../store/request-robots";
 
 const CardList = ({ robots }: IRobotState) => {
